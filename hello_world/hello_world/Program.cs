57627bb0a8efcb7b8348bc1fc37100fe690b10db
CaptMurphy <adam.l.vincent@gmail.com>
2017-10-23 10:04:26 -0400

    c# hello_world

---
+++ hello_world/hello_world/Program.cs
@@ -0,0 +1,16 @@
+﻿//AUTHOR: Adam Vincent (CaptMurphy)
+//LANGUAGE: C#
+//GITHUB: https://github.com/captmurphy
+
+using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
