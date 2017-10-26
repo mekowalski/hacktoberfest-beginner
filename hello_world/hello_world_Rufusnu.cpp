5b6b98c1aa8605d4693f1d75dffab1758332bef5
Rufusnu <rufusnu@yahoo.com>
2017-10-26 20:41:13 +0300

    C++ hello world

---
+++ hello_world/hello_world_Rufusnu.cpp
@@ -0,0 +1,12 @@
+// AUTHOR: Sofian Razvan
+// LANGUAGE: C++
+// GITHUB: https://github.com/Rufusnu
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World from Romania!" << endl;
+    return 0;
+}
