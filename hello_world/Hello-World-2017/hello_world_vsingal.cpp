a4d9d69247f68ae0ccbaa3da80e0e616016735fb
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-10 17:52:09 -0600

    remove merge matter

--- hello_world/Hello-World-2017/hello_world_vsingal.cpp
+++ hello_world/Hello-World-2017/hello_world_vsingal.cpp
@@ -1,15 +1,11 @@
-<<<<<<< HEAD:hello_world/hello.cpp
-//AUTHOR: virendrasingal
-=======
 //AUTHOR: Virendra Singal
->>>>>>> master:hello_world/Hello-World-2017/hello_world_vsingal.cpp
 //LANGUAGE: C++
 //GITHUB: https://github.com/virendrasingal
 
 #include <iostream>
 using namespace std;
 
-int main() 
+int main()
 {
     cout << "Hello, World!";
     return 0;
