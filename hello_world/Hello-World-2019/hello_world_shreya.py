23ad31fcf2b665c89948d7de7b90dc5dc9f3301c
Malind Kowalski <malind.nikulski@gmail.com>
2019-10-02 14:30:21 -0600

    move file to different DIR and reformat

---
+++ hello_world/Hello-World-2019/hello_world_shreya.py
@@ -0,0 +1,6 @@
+#AUTHOR: Shreya
+#LANGUAGE: Python
+#GITHUB: https://github.com/shreya2211
+
+print("Hello, World!")
+print("nice meeting you")
--- hello_world/Hello-world-2019/hello_world_shreya.py
+++
@@ -1,6 +0,0 @@
-// AUTHOR: shreya
-// LANGUAGE: python
-// GITHUB: https://github.com/shreya2211
-
-print("Hello, World!")
-print("nice meeting you")
