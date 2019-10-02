cb661d472684e3d0ce57b073d0f8f8df50629d65
Shrikha <mahanty.shrikha@gmail.com>
2019-10-02 16:33:43 +0530

    Add hello_world.cpp

---
+++ hello_world/Hello-World-2019/hello_world.cpp
@@ -0,0 +1,7 @@
+#include<iostream>                              //including pre-processor for using cout.
+
+int main()                                      // Starting main function.
+{
+    std::cout<<"Hello World!!"<<std::endl;      // Display string "Hello World!!" on the console.
+    return 0;                                   // return int 0 when the program executed successfully.
+}                                                
