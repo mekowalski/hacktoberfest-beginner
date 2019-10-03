abf9f2ce11337a38eeb0851fc226522754b4cfb2
JEvason90 <j.evason1349@gmail.com>
2019-10-03 18:47:21 +0100

    Added my profile and Hello World

---
+++ hello_world/Hello-World-2019/hello_world_jevason.cs
@@ -0,0 +1,16 @@
+//AUTHOR: Jonathan Evason
+//LANGUAGE: C#
+//GITHUB: https://github.com/Jevason90
+
+using System;
+
+namespace hello_world_jevason
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
