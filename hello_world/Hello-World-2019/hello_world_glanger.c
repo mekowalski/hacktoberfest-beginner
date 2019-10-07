47a0f152d1a115b3dc621f8719ca194a62630a40
Malind Kowalski <malind.nikulski@gmail.com>
2019-10-07 10:14:58 -0600

    moved and formatted file

--- hello_world/Hello-World-2019/hello_world_glanger.c
+++ hello_world/Hello-World-2019/hello_world_glanger.c
@@ -1,14 +1,13 @@
-AUTHOR: Gabriel Langer
-LANGUAGE: C
-GITHUB: https://github.com/gablanger
+// AUTHOR: Gabriel Langer
+// LANGUAGE: C
+// GITHUB: https://github.com/gablanger
 
 #include <stdio.h>
 
 int main() {
   printf("Hello World\n");
 
-
   //added a hindi word Namaskar, used to greet people in India.
-  printf("Namaskar\n"); 
+  printf("Namaskar\n");
   return 0;
 }
