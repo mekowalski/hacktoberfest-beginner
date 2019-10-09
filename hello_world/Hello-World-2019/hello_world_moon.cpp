5d15064c53bd007a44ad3b631ffccb5e909ccb02
Morgen Sudyanto <moondemon68@gmail.com>
2019-10-09 13:56:10 +0700

    my commit

---
+++ hello_world/Hello-World-2019/hello_world_moon.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define MOD 1000000007
+#define INF 1234567890
+#define pii pair<int,int>
+#define LL long long
+using namespace std;
+
+int main () {
+    //clock_t start = clock();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout << "Hello World!" << endl;
+    //cerr << fixed << setprecision(3) << (clock()-start)*1./CLOCKS_PER_SEC << endl;
+    return 0;
+}
