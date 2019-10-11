5913b8c700ee47ff32d9fe7bf0b31b139cd45a4b
Shivakant Vishwakarma <30767792+shivakant1999@users.noreply.github.com>
2019-10-11 12:25:51 +0530

    create hello_world_shivakant1999.cpp

---
+++ hello_world/Hello-World-2019/hello_world_shivakant1999.cpp
@@ -0,0 +1,11 @@
+#AUTHOR: Shivakant Vishwakarma
+#LANGUAGE: C++
+#GITHUB: https://github.com/shivakant1999
+
+include<iostream>
+using namespace std;
+int main()
+{
+cout << "Hello World\n";
+return 0;
+}
