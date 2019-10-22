fdbc5fc737009950cc6d514852a89ebd88b55e80
snehit31 <46548060+snehit31@users.noreply.github.com>
2019-10-22 23:28:17 +0530

    Create snehit.cpp
    My addition into hello world program.

---
+++ hello_world/Hello-World-2019/snehit.cpp
@@ -0,0 +1,13 @@
+// AUTHOR:Snehit Mishra
+// LANGUAGE: C++
+// GITHUB: https://github.com/Amitdutta7
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hi!!\n Welcome\nHello World" <<"\n";
+    return 0;
+}
