5303cd1eba6f2398e8a4b35b434506065e1a7788
Uvindu Harshana <d-folt@pop-os.localdomain>
2020-10-11 09:20:43 +0530

    Added HelloWorld in cpp by Uvindu Harshana

---
+++ hello_world/Hello-World-2020/hello_world_uharshana.cpp
@@ -0,0 +1,11 @@
+// AUTHOR: Uvindu Harshana
+// LANGUAGE: Java
+// GITHUB: https://github.com/d-folt
+
+#include<iostream>;
+using namespace std;
+
+int main() {
+    cout<<"Hello, World!"<<endl;
+    return 0;
+}
