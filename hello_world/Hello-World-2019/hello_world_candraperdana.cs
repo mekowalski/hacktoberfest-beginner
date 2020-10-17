ca3489e50c0749445b5a6a0b48dce7cc06df857a
condro <anukumu099@gmail.com>
2020-10-17 22:24:25 +0700

    candra-perdana

---
+++ hello_world/Hello-World-2019/hello_world_candraperdana.cs
@@ -0,0 +1,20 @@
+// AUTHOR: Candra Perdana
+// LANGUAGE: C#
+// GITHUB: https://github.com/cp2940
+
+ using System;
+
+namespace HelloWorl {
+
+    class Hello {
+
+        static void Main(string[] args) {
+
+            Console.WriteLine("Hello World!");
+
+            // To prevents the screen from
+            // running and closing quickly
+            Console.ReadKey();
+        }
+    }
+}
---
+++ profiles/Profiles-2019/Candra_Perdana.md
@@ -0,0 +1,22 @@
+# Candra Perdana
+
+### Location
+Surakarta,Jawa Tengah,Indonesia
+
+### Academics
+- University Muhammadiyah Surakarta
+- Mojolaban High School
+
+### Non-Coding Interests
+- Music
+- Gaming
+
+### Why Do You Love Coding?
+Because coding is really fun and fill my free time
+
+### Your Favorite Animal
+Cat
+
+### Your Favorite Dessert
+Tiramisu
+
