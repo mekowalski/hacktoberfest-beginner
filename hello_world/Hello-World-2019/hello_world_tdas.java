a3648c894c9b894642140131c8e2c0cabea872d4
Thanmay <mdasthanmay@gmail.com>
2021-10-27 21:57:16 +0530

    Add Tan

---
+++ hello_world/Hello-World-2019/hello_world_tdas.java
@@ -0,0 +1,9 @@
+// AUTHOR: Thanmay M Das
+// LANGUAGE: Java
+// GITHUB: https://github.com/tAnboyy
+
+public class hello_world_tdas {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
