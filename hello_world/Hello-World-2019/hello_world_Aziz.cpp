6430e857aaff3fefa34f311a418c96333ba2e0ad
Abdul Aziz <abdul20485@iiit.ac.in>
2023-10-11 18:38:31 +0530

    'v1'

---
+++ hello_world/Hello-World-2019/hello_world_Aziz.cpp
@@ -0,0 +1,12 @@
+/* 
+   AUTHOR: Gaurav Singh
+   LANGUAGE: C++
+   GITHUB: https://github.com/saintpopo
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    cout << "Hello World" << endl;
+    return 0;
+}
---
+++ profiles/Profiles-2019/Abdul_Aziz_Mustafa.md
@@ -0,0 +1,15 @@
+#  Abdul Aziz 
+###  Location
+Ranchi,Jharkhand,India
+
+### Academics
+- IIITD
+
+###  Non-Coding Interests
+- Music
+- Sports
+
+
+###  Your Favorite Animal
+CAT
+
