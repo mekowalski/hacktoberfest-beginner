7f6a2a7c49a069d63820c64085333649489915f2
sudhanshu576 <sudhanshuroy576@gmail.com>
2024-02-09 22:45:27 +0530

    added my hello world file

---
+++ hello_world/Hello-World-2019/hello_world_sudhanshu.java
@@ -0,0 +1,9 @@
+//Auther: Sudhanshu kumar roy
+//Language : java
+//GitHub : https://github.com/sudhanshu576
+
+class Sudhanshu{
+    public static void main(String[] args) {
+        System.out.println("Hello world !");
+    }
+}
